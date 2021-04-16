6be933349ab183d4055e5d301f92f27499094dbd
sassan-mobiquity <sassan_sadeghi@yahoo.com>
2021-04-17 09:59:31 +1000

    configured the project in test runner

--- src/test/java/com/mobiquity/assignment/TestRunnerBase.java
+++ src/test/java/com/mobiquity/assignment/TestRunnerBase.java
@@ -1,6 +1,8 @@
 package com.mobiquity.assignment;
 
 
+import com.typesafe.config.Config;
+import com.typesafe.config.ConfigFactory;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.BeforeSuite;
@@ -14,15 +16,21 @@ import java.util.Date;
 public class TestRunnerBase extends AbstractTestNGCucumberTests {
 
     private static final String timeStamp = new SimpleDateFormat("yyyy-MM-dd-hh-mm-ss").format(new Date());
+    private static TestConfig props;
 
     @BeforeSuite
     void beforeSuite() {
         log.info("starting the test suite at: " + timeStamp);
+        log.info("configuring the project");
+        Config profile = ConfigFactory.load("local-config.conf");
+        Config envOverrides = ConfigFactory.systemProperties()
+                .withFallback(ConfigFactory.systemEnvironment());
+        props = new TestConfig(envOverrides.withFallback(profile).resolve());
     }
 
     @Test
     void test() {
-        log.info("running the test");
+        log.info("running api tests against: " + props.getTypicodeURL());
     }
 
 }
