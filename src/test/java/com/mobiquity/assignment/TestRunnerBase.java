86d909553c50ed543e78df13939ed25ea21ae22a
sassan-mobiquity <sassan_sadeghi@yahoo.com>
2021-04-17 16:28:07 +1000

    removed the test from the base class

--- src/test/java/com/mobiquity/assignment/TestRunnerBase.java
+++ src/test/java/com/mobiquity/assignment/TestRunnerBase.java
@@ -4,27 +4,34 @@ package com.mobiquity.assignment;
 import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-import io.restassured.response.Response;
+import io.cucumber.testng.CucumberOptions;
 import lombok.extern.slf4j.Slf4j;
 import org.testng.annotations.BeforeSuite;
-import org.testng.annotations.Test;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import static com.mobiquity.assignment.RestClientBase.usersEndpoint;
-import static io.restassured.RestAssured.given;
-
 
 @Slf4j
+@CucumberOptions()
 public class TestRunnerBase extends AbstractTestNGCucumberTests {
 
     private static final String timeStamp = new SimpleDateFormat("yyyy-MM-dd-hh-mm-ss").format(new Date());
     private static TestConfig props;
 
     @BeforeSuite
-    void beforeSuite() {
-        log.info("starting the test suite at: " + timeStamp);
+    public void beforeSuite() {
+        config();
+    }
+
+
+    /**
+     * Configures the project based on the following priority:
+     * 1- System properties
+     * 2- System Environment Variables
+     * 3- Local config file
+     */
+    public static void config() {
         log.info("configuring the project");
         Config profile = ConfigFactory.load("local-config.conf");
         Config envOverrides = ConfigFactory.systemProperties()
@@ -33,14 +40,5 @@ public class TestRunnerBase extends AbstractTestNGCucumberTests {
         RestClientBase.init(props.getDefaultTimeoutMilSec(), props.getTypicodeURL());
     }
 
-    @Test
-    void test() {
-        log.info("running api tests against: " + props.getTypicodeURL());
-        Response resp = given().
-                queryParam("username", "Delphine").
-                when().
-                get(usersEndpoint);
-        resp.prettyPrint();
-    }
 
 }
