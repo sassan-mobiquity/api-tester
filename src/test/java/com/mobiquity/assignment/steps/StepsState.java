955ba70d80865d4f18698ea669af5eb09c2b6e8a
sassan-mobiquity <sassan_sadeghi@yahoo.com>
2021-04-17 17:18:33 +1000

    created step states

---
+++ src/test/java/com/mobiquity/assignment/steps/StepsState.java
@@ -0,0 +1,9 @@
+package com.mobiquity.assignment.steps;
+
+import java.util.ArrayList;
+
+public class StepsState {
+    public ArrayList<Integer> postIds;
+    public ArrayList<String> emails;
+    public String userId;
+}
